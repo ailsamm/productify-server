3a15855d903df26c42dc5d3ffbf1a0d630273e67
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-05-29 15:51:12 -0700

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,32 @@
+const bookmarks = [
+    {
+        "id": "cjozyzcil0000lxygs3gyg2mr",
+        "title": "Thinkful",
+        "url": "https://www.thinkful.com",
+        "description": "Think outside the classroom",
+        "rating": 5
+    },
+    {
+        "id": "cjozyzeqh0001lxygb8mhnvhz",
+        "title": "Google",
+        "url": "https://www.google.com",
+        "description": "Where we find everything else",
+        "rating": 4
+    },
+    {
+        "id": "cjkzyzeqh0001lxygb8mhqvh3",
+        "title": "MDN",
+        "url": "https://developer.mozilla.org",
+        "description": "The only place to find web documentation",
+        "rating": 5
+    },
+    {
+        "id": "ck9vrii2m000004uzhlnbs3ty",
+        "title": "news",
+        "url": "https://cnn.com",
+        "description": "",
+        "rating": 1
+    }
+]
+
+module.exports = bookmarks
