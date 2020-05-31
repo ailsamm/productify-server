59920306b49464e8cde156eb4915f12861cd6375
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-05-30 21:03:36 -0700

    Add teams router and service

---
+++ src/users/teams-router.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const teamsService = require('./teams-service')
+const teamsRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeTeam = team => ({
+  id: team.id,
+  team_name: xss(team.team_name),
+});
+
+teamsRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    
+    teamsService.getAllTeams(knexInstance)
+      .then(teams => {
+        res.json(teams.map(serializeTeam))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { team_name, id } = req.body
+    const newTeam = { team_name, id }
+
+    for (const [key, value] of Object.entries({ team_name, id })) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+      }
+    }
+
+    teamsService.insertTeam(
+      req.app.get('db'),
+      newTeam
+    )
+      .then(team => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${team.id}`))
+          .json(serializeTeam(team))
+      })
+      .catch(next)
+  })
+
+  teamsRouter
+  .route('/:team_id')
+  .all((req, res, next) => {
+    teamsService.getById(
+      req.app.get('db'),
+      req.params.team_id
+    )
+      .then(team => {
+        if (!team) {
+          return res.status(404).json({
+            error: { message: `Team doesn't exist` }
+          })
+        }
+        res.team = team
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeTeam(res.team))
+  })
+  .delete((req, res, next) => {
+    teamsService.deleteTeam(
+      req.app.get('db'),
+      req.params.team_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { team_name } = req.body
+    const teamToUpdate = { team_name }
+
+    const numberOfValues = Object.values(teamToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain 'team_name'`
+        }
+      })
+
+    teamsService.updateTeam(
+      req.app.get('db'),
+      req.params.team_id,
+      teamToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = teamsRouter
---
+++ src/users/teams-service.js
@@ -0,0 +1,34 @@
+const TeamsService = {
+    getAllTeams(knex) {
+      return knex.select('*').from('productify_teams')
+    },
+    insertTeam(knex, newTeam){
+      return knex
+          .insert(newTeam)
+          .into('productify_teams')
+          .returning('*')
+          .then(rows => {
+            console.log(rows)
+            return rows[0]
+      })
+    },
+    deleteTeam(knex, id){
+      return knex('productify_teams')
+          .where({ id })
+          .delete()
+    },
+    getById(knex, id){
+      return knex
+        .from('productify_teams')
+        .select('*')
+        .where('id', id)
+        .first()
+    },
+    updateTeam(knex, id, newTeamFields) {
+      return knex('productify_teams')
+        .where({ id })
+        .update(newTeamFields)
+    }
+  }
+  
+  module.exports = TeamsService;
