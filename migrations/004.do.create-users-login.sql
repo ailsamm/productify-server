f3d7d3b766615149a8500c917e855d3e371d3410
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-01 11:45:13 -0700

    Update migrations to allow for UUID identification

--- migrations/004.do.create-users-login.sql
+++ migrations/004.do.create-users-login.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS productify_users_login;
 
 CREATE TABLE productify_users_login (
-    user_id INTEGER REFERENCES productify_users_info(id) ON DELETE CASCADE NOT NULL,
+    user_id TEXT REFERENCES productify_users_info(id) ON DELETE CASCADE NOT NULL,
     email_address TEXT NOT NULL,
     password TEXT NOT NULL
 );
