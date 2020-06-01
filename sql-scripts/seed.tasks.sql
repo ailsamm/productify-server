c0301edfb687084e63047c5d57b9d1e2c4789cf8
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-01 11:57:51 -0700

    Make IDs integers

--- sql-scripts/seed.tasks.sql
+++ sql-scripts/seed.tasks.sql
@@ -4,7 +4,7 @@ VALUES
     (1, 'Deploy app', 'blah...', '2020-06-29T19:29:21.212Z', 'backlog', 1),
     (1, 'Set up server', 'blah...', '2020-06-06T19:29:21.212Z', 'inProgress', 3),
     (1, 'Create endpoints', 'blah...', '2020-06-10T19:29:21.212Z', 'inReview', 4),
-    (1, 'Add CRUD functionality', 'blah...', '2020-06-25T19:29:21.212Z', 'complete', 5),
-    (2, 'Make endpoints protected', 'blah...', '2020-06-21T19:29:21.212Z', 'complete', 6),
+    (1, 'Add CRUD functionality', 'blah...', '2020-06-25T19:29:21.212Z', 'complete', 6),
+    (2, 'Make endpoints protected', 'blah...', '2020-06-21T19:29:21.212Z', 'complete', 5),
     (2, 'Test with small group', 'blah...', '2020-06-20T19:29:21.212Z', 'backlog', 1),
     (2, 'Add animations to UI', 'blah...', '2020-06-10T19:29:21.212Z', 'inProgress', 2);
